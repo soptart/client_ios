23aac2f277007408ec632310d45c7c03a5ff279b
jmzzang <dream7739@naver.com>
2018-12-22 16:57:13 +0900

    gitignore 추가

---
+++ Artoo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artoo
+//
+//  Created by 홍정민 on 2018. 12. 22..
+//  Copyright © 2018년 홍정민. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
