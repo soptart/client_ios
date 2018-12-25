e936e92b90dd9e56b08f8988fbceb47e482887f6
jmzzang <dream7739@naver.com>
2018-12-25 15:53:46 +0900

    전시 / 메인 UI 구성(테이블뷰 연결X)

---
+++ Artoo/VC/Exhibit/ExhibitCell.swift
@@ -0,0 +1,14 @@
+//
+//  ExhibitCell.swift
+//  Artoo
+//
+//  Created by 홍정민 on 2018. 12. 25..
+//  Copyright © 2018년 홍정민. All rights reserved.
+//
+
+import UIKit
+
+class ExhibitCell: UITableViewCell {
+    @IBOutlet weak var exhibitImg: UIImageView!
+    
+}
