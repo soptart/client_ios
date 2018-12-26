efd58609ff84fb939af579515dbd1ce307fd2bf3
jmzzang <dream7739@naver.com>
2018-12-26 21:21:06 +0900

    전시 / 메인UI 완료

---
+++ Artoo/Util/Model/Exhibit.swift
@@ -0,0 +1,15 @@
+//
+//  Exhibit.swift
+//  Artoo
+//
+//// 전시 모델
+
+import Foundation
+
+struct Exhibit {
+
+    ////모델 수정해야함
+    let exhibitText:String
+    let exhibitDate:String
+    let exhibitImg:[String]
+}
--- Artoo/VC/Exhibit/ExhibitVC.swift
+++ Artoo/VC/Exhibit/ExhibitVC.swift
@@ -9,13 +9,80 @@
 import UIKit
 
 class ExhibitVC: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //상단 전시설명 라벨
+    @IBOutlet weak var exhibitLabel: UILabel!
+    
+    //상단 전시 날짜
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    //상단 전시 버튼 -> 클릭시 새 창
+    @IBOutlet weak var exhibitBtn: UIButton!
+    
+    //쓰레기 데이터
+    var exhibitList: [Exhibit] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        setData()
     }
     
+}
 
-  
+extension ExhibitVC : UITableViewDelegate {
+    
+}
+
+extension ExhibitVC : UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let section =  exhibitList.first?.exhibitImg.count else {
+            return 1
+        }
+        
+        return section
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ExhibitCell") as! ExhibitCell
+        if let data = exhibitList.first?.exhibitImg[indexPath.row] {
+            cell.exhibitImg.image = UIImage(named: data)
+        } else {
+            //전시이미지가 없을 시에 이미지
+            cell.exhibitImg.image = UIImage(named: "ggobuk")}
+        return cell
+
+    }
+    
+    
+}
+
+extension ExhibitVC {
+    
+    //초기 데이터 세팅
+    func setData(){
+        exhibitList.append(Exhibit(exhibitText: "11월의 단체전시 신청시작! \n[익숙함이 새로웠던 전] - 일상편", exhibitDate: "2019.1.2~2019.2.3", exhibitImg:["exhibit","exhibit","exhibit","exhibit"]))
+        
+        if let initData = exhibitList.first {
+            exhibitLabel.text = initData.exhibitText
+            dateLabel.text = initData.exhibitDate
+        }else {
+            exhibitLabel.text = "전시 이름"
+            dateLabel.text = "전시 날짜"
+        }
+    }
+    
+    
+    
+    
 }
