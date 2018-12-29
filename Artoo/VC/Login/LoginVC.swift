ce6f7f87435f11418002cb760c6be437c8a93a75
jmzzang <dream7739@naver.com>
2018-12-29 13:12:34 +0900

    메인 / 로그인 회원가입 UI구성

---
+++ Artoo/VC/Login/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  Artoo
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    @IBOutlet weak var loginBtn: UIButton!
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
+
