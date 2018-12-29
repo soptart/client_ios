ceea9c76e46ca95243585d61c44d34e1c87715a3
보윤 <bykim94@hanmail.net>
2018-12-30 08:55:10 +0900

    tap success!!

---
+++ Artoo/VC/Art/DealVC.swift
@@ -0,0 +1,80 @@
+//
+//  DealVC.swift
+//  Artoo
+//
+//  Created by 보윤 on 30/12/2018.
+//  Copyright © 2018 홍정민. All rights reserved.
+//
+
+import UIKit
+
+class DealVC: UIViewController {
+
+    @IBOutlet weak var deliveryBtn: UIButton!
+    @IBOutlet weak var directBtn: UIButton!
+    @IBOutlet weak var containerView: UIView!
+    
+    
+    //택배나 직거래 버튼 눌렀을 때 화면이 변하기
+    @IBAction func changeView(_ sender: UIButton) {
+        updateView(selected: sender.tag)
+    }
+    
+    private lazy var deliveryService: DeliveryVC = {
+        let storyboard = Storyboard.shared().artStoryboard
+        
+        var viewController = storyboard.instantiateViewController(withIdentifier: "delivery") as! DeliveryVC
+        
+        self.add(asChildViewConroller: viewController)
+        
+        return viewController
+    }()
+    
+    private lazy var directService: DirectVC = {
+        let storyboard = Storyboard.shared().artStoryboard
+        
+        var viewController = storyboard.instantiateViewController(withIdentifier: "direct") as! DirectVC
+        
+        self.add(asChildViewConroller: viewController)
+        
+        return viewController
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+//DealVC의 확장 기능
+extension DealVC {
+    
+    private func add(asChildViewConroller viewController: UIViewController) {
+        
+        addChild(viewController)
+        containerView.addSubview(viewController.view)
+        
+        viewController.view.frame = containerView.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        viewController.didMove(toParent: self)
+    }
+    
+    private func remove(asChildViewController viewController: UIViewController){
+        viewController.willMove(toParent: nil)
+        viewController.view.removeFromSuperview()
+        viewController.removeFromParent()
+    }
+    
+    private func updateView(selected: Int){
+        if selected == 0{
+            remove(asChildViewController: directService)
+            add(asChildViewConroller: deliveryService)
+        } else {
+            remove(asChildViewController: deliveryService)
+            add(asChildViewConroller: directService)
+        }
+    }
+}
---
+++ Artoo/VC/Art/DeliveryVC.swift
@@ -0,0 +1,30 @@
+//
+//  DeliveryVC.swift
+//  Artoo
+//
+//  Created by 보윤 on 30/12/2018.
+//  Copyright © 2018 홍정민. All rights reserved.
+//
+
+import UIKit
+
+class DeliveryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
