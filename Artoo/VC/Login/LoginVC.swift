f6ee375adcbd3396170ba792b0bd27d26f428b03
jmzzang <dream7739@naver.com>
2018-12-29 17:01:12 +0900

    메인 tab 이동

--- Artoo/VC/Login/LoginVC.swift
+++ Artoo/VC/Login/LoginVC.swift
@@ -10,11 +10,19 @@ class LoginVC: UIViewController {
     @IBOutlet weak var loginBtn: UIButton!
 
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loginBtn.addTarget(self, action: #selector(goTab), for: .touchUpInside)
     }
     
+}
+extension LoginVC {
+    
+    @objc func goTab(){
+        let appDelegate = UIApplication.shared.delegate! as! AppDelegate
+        let initialViewController = self.storyboard!.instantiateViewController(withIdentifier: "tab")
+        appDelegate.window?.rootViewController = initialViewController
+        appDelegate.window?.makeKeyAndVisible()
+    }
 }
 
