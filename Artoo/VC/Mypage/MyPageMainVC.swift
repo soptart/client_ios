1f9c41105f755783c0feac306053ab13acdc12ce
보윤 <bykim94@hanmail.net>
2018-12-30 17:18:57 +0900

    tap success!

--- Artoo/VC/Mypage/MyPageMainVC.swift
+++ Artoo/VC/Mypage/MyPageMainVC.swift
@@ -12,12 +12,19 @@ class MyPageMainVC: UIViewController {
 
     @IBOutlet weak var MainIntroductionLabel: UILabel!
     @IBOutlet weak var EditTextView: UITextView!
+    @IBOutlet weak var artBtn: UIButton!
+    @IBOutlet weak var saveBtn: UIButton!
+    @IBOutlet weak var buyBtn: UIButton!
+    @IBOutlet weak var reviewBtn: UIButton!
+    @IBOutlet weak var containerView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         EditTextView.isScrollEnabled = false
+        updateView(selected: 0)
     }
     
     
@@ -33,10 +40,99 @@ class MyPageMainVC: UIViewController {
         MainIntroductionLabel.textColor = UIColor.darkGray
     }
     
-    
+    //버튼에 따라서 아래 뷰가 바뀌기.
     @IBAction func changeView(_ sender: UIButton) {
-        
+        updateView(selected: sender.tag)
     }
+    
+    //art_page리턴
+    private lazy var artPage: MyPageArtVC = {
+        let storyboard = Storyboard.shared().mypageStoryboard
+        
+        var viewController = storyboard.instantiateViewController(withIdentifier: "art_page") as! MyPageArtVC
+        
+        self.add(asChildViewController: viewController)
+        
+        return viewController
+    }()
+    
+    //store_page리턴
+    private lazy var storePage: MyPageStoreVC = {
+        let storyboard = Storyboard.shared().mypageStoryboard
+        
+        var viewController = storyboard.instantiateViewController(withIdentifier: "store_page") as! MyPageStoreVC
+        
+        self.add(asChildViewController: viewController)
+        
+        return viewController
+    }()
+    
+    
+    //buy_page리턴
+    private lazy var buyPage: MyPageBuyVC = {
+        let storyboard = Storyboard.shared().mypageStoryboard
+        
+        var viewController = storyboard.instantiateViewController(withIdentifier: "buy_page") as! MyPageBuyVC
+        
+        self.add(asChildViewController: viewController)
+        
+        return viewController
+    }()
+    
+    //review_page리턴
+    private lazy var reviewPage: MyPageReviewVC = {
+        let storyboard = Storyboard.shared().mypageStoryboard
+        
+        var viewController = storyboard.instantiateViewController(withIdentifier: "review_page") as! MyPageReviewVC
+        
+        self.add(asChildViewController: viewController)
+        
+        return viewController
+    }()
     
 }
 
+extension MyPageMainVC {
+    
+    private func add(asChildViewController viewController: UIViewController) {
+        
+        addChild(viewController)
+        containerView.addSubview(viewController.view)
+        
+        viewController.view.frame = containerView.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        viewController.didMove(toParent: self)
+    }
+    
+    private func remove(asChildViewController viewController: UIViewController) {
+        
+        viewController.willMove(toParent: nil)
+        viewController.view.removeFromSuperview()
+        viewController.removeFromParent()
+    }
+    
+    private func updateView(selected: Int){
+        if selected == 0 {
+            remove(asChildViewController: storePage)
+            remove(asChildViewController: buyPage)
+            remove(asChildViewController: reviewPage)
+            add(asChildViewController: artPage)
+        } else if selected == 1 {
+            remove(asChildViewController: artPage)
+            remove(asChildViewController: buyPage)
+            remove(asChildViewController: reviewPage)
+            add(asChildViewController: storePage)
+        } else if selected == 2 {
+            remove(asChildViewController: artPage)
+            remove(asChildViewController: storePage)
+            remove(asChildViewController: reviewPage)
+            add(asChildViewController: buyPage)
+        } else {
+            remove(asChildViewController: artPage)
+            remove(asChildViewController: storePage)
+            remove(asChildViewController: buyPage)
+            add(asChildViewController: reviewPage)
+        }
+    }
+}
