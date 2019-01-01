2cb747b191072cf874608dc193c6bc3c1c9b2a38
보윤 <bykim94@hanmail.net>
2019-01-01 20:35:53 +0900

    알림창 띄우기는 성공(다른 페이지로 이동하기는 아직 안됨)

---
+++ Artoo/VC/Mypage/NotifyUploadVC.swift
@@ -0,0 +1,36 @@
+//
+//  NotifyUploadVC.swift
+//  Artoo
+//
+//  Created by 보윤 on 01/01/2019.
+//  Copyright © 2019 홍정민. All rights reserved.
+//
+
+import UIKit
+
+class NotifyUploadVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+ 
+    @IBAction func OkBtn(_ sender: Any) {
+        
+          guard let uploadVC = storyboard?.instantiateViewController(withIdentifier: "uploadMain") as? UploadMainVC else{ return }
+        /*
+        dismiss(animated: true, completion: {()-> Void in self.present(uploadVC, animated: true, completion: nil)})
+        */
+        
+        dismiss(animated: true, completion: {()-> Void in self.navigationController?.pushViewController(uploadVC, animated: true)})
+        
+        }
+
+    }
+
+        /*
+    func move() {
+        
+        guard let uploadVC = storyboard?.instantiateViewController(withIdentifier: "uploadMain") as? UploadMainVC else{ return }
+        navigationController?.pushViewController(uploadVC, animated: true)
+    }*/
---
+++ Artoo/VC/Mypage/UploadMainVC.swift
@@ -0,0 +1,19 @@
+//
+//  UploadMainVC.swift
+//  Artoo
+//
+//  Created by 보윤 on 01/01/2019.
+//  Copyright © 2019 홍정민. All rights reserved.
+//
+
+import UIKit
+
+class UploadMainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+}
