58a7759f95bb936dff5c83fe7ea37e6ca58b2399
보윤 <bykim94@hanmail.net>
2019-01-03 11:21:58 +0900

    알림 셀 선택 제외하고 성공

---
+++ Artoo/Util/Model/sellerInfo.swift
@@ -0,0 +1,16 @@
+//
+//  sellerInfo.swift
+//  Artoo
+//
+//  Created by 보윤 on 03/01/2019.
+//  Copyright © 2019 홍정민. All rights reserved.
+//
+
+import Foundation
+struct sellerInfo {
+    
+    var sellerName: String!
+    var sellerPhone: String!
+    var sellerAddr: String!
+    
+}
