2b0b2cb172e4076991bf3880b4de0565f9c1d4a1
jmzzang <dream7739@naver.com>
2019-01-05 04:01:01 +0900

    홈 투데이 UI 변경

--- Artoo/VC/Home/Today/HomeTodayVC.swift
+++ Artoo/VC/Home/Today/HomeTodayVC.swift
@@ -24,7 +24,6 @@ class HomeTodayVC: UIViewController {
         
         //서버 통신해서 todayList 채우기
         getToday()
-        
         //서버정보가 아닌 시리즈 정보들(디자이너 데이터)
         seriesTableView.delegate = self
         seriesTableView.dataSource = self
@@ -60,105 +59,105 @@ extension HomeTodayVC : UITableViewDataSource {
 
 ////CollectionView를 위한 Extension
 extension HomeTodayVC : UICollectionViewDataSource {
-
-
+    
+    
     //섹션 당 아이템 개수
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-
+        
         if collectionView == authorCollection {
             return todayList.count
         }else {
             return (todayList[authorIndex].authorWork?.count)!
         }
-
+        
     }
-
+    
     //섹션의 개수
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-
+    
     //cell에 데이터 세팅
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
-
+        
+        
         switch collectionView {
         case authorCollection:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AuthorCell", for: indexPath) as! TodayAuthorCell
-
+            
             //작가 이름
             let todayData = todayList[indexPath.row]
             cell.authorName.text = todayData.authorName
             
             return cell
-
+            
         case workCollection:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WorkCell", for: indexPath) as! TodayWorkCell
-
+            
             let workInfo = todayList[authorIndex] //작가에 대한 대략적 정보
             let detailWorkInfo = workInfo.authorWork![indexPath.row]
-
+            
             let photoUrl = detailWorkInfo.workImg!
             cell.workImg.imageFromUrl(photoUrl, defaultImgPath: "ggobuk")
-
+            
             if(indexPath.row != 0){
                 cell.authorIntro.isHidden = true
                 cell.authorName.isHidden = true
-                cell.arrowBtn.isHidden = true
+                cell.schoolLabel.isHidden = true
+                cell.img.isHidden = true
+                cell.arrowImg.isHidden = true
             }else {
                 cell.authorIntro.isHidden = false
                 cell.authorName.isHidden = false
-                cell.arrowBtn.isHidden = false
+                cell.schoolLabel.isHidden = false
+                cell.img.isHidden = false
+                cell.arrowImg.isHidden = false
 
+                
                 cell.authorName.text = workInfo.authorName
-
+                
                 if let intro = workInfo.authorIntro {
                     cell.authorIntro.text = intro
                 }else{
                     cell.authorIntro.text = ""
                 }
             }
-
+            
             return cell
         default:
             return UICollectionViewCell()
         }
-
+        
     }
-
-
+    
+    
 }
 
 
 extension HomeTodayVC : UICollectionViewDelegateFlowLayout {
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-
+        
         //사이즈 재조정 필요
         switch collectionView {
         case authorCollection:
-            let width = (view.frame.width) / 6
-            let height = (view.frame.height) / 9
-            return CGSize(width: width, height: height)
-
+            return CGSize(width: 44, height: 24)
         case workCollection:
-            let width = (view.frame.width - 15) / 1.6
-            let height = (view.frame.height) / 2  - 38.6
-            return CGSize(width: width, height: height)
+            return CGSize(width: 250, height: 347)
         default:
             return CGSize(width: 0, height: 0)
         }
-
-
+        
+        
     }
-
+    
     //하나의 행에 있는 아이템들의 가로간격
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
+        return 10
     }
-
-
-
+    
+    
+    
     //섹션 내부 여백
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         switch collectionView {
@@ -170,27 +169,27 @@ extension HomeTodayVC : UICollectionViewDelegateFlowLayout {
         default:
             return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         }
-
+        
     }
-
+    
     //콜렉션 뷰 아이템 클릭 시 이벤트
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-
+        
         switch collectionView {
-
+            
         case authorCollection:
             authorIndex = indexPath.row
             workCollection.reloadData()
             //스크롤 포지션을 처음으로 되돌림
             workCollection.contentOffset.x = 0
         case workCollection:
-           //작품창으로 이동
+            //작품창으로 이동
             print("hihi")
         default:
             return
         }
     }
-
+    
 }
 
 extension HomeTodayVC {
--- Artoo/VC/Home/Today/TodayWorkCell.swift
+++ Artoo/VC/Home/Today/TodayWorkCell.swift
@@ -7,12 +7,21 @@ import UIKit
 
 class TodayWorkCell: UICollectionViewCell {
     @IBOutlet weak var workImg: UIImageView!
-    
-    
+
+    //작가 이름
     @IBOutlet weak var authorName: UILabel!
     
+    //학교 이름
+    @IBOutlet weak var schoolLabel: UILabel!
+    
+    //작가 소개
     @IBOutlet weak var authorIntro: UILabel!
     
-    @IBOutlet weak var arrowBtn: UIImageView!
+    //-- 이미지
+    @IBOutlet weak var img: UIImageView!
+    
+    //화살표 이미지
+    @IBOutlet weak var arrowImg: UIImageView!
+    
     
 }
