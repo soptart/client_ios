2e7f2f2e515d126ba1fb5351cb6bc4ba2703fba9
보윤 <bykim94@hanmail.net>
2019-01-04 22:19:43 +0900

    ok,cancel 버튼 성공

--- Artoo/VC/Mypage/exhibitCancelNoticeVC.swift
+++ Artoo/VC/Mypage/exhibitCancelNoticeVC.swift
@@ -16,15 +16,9 @@ class exhibitCancelNoticeVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func okBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
 
 }
