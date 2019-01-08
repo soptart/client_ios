6d1e5b5a6db5560a987e3bdfa0c66a0563d0d51a
jmzzang <dream7739@naver.com>
2019-01-08 15:09:02 +0900

    팝업 2개 오토레이아웃

---
+++ Artoo/VC/Exhibit/ApplyFailVC.swift
@@ -0,0 +1,30 @@
+//
+//  ApplyFailVC.swift
+//  Artoo
+//
+//  Created by 홍정민 on 2019. 1. 8..
+//  Copyright © 2019년 홍정민. All rights reserved.
+//
+
+import UIKit
+
+class ApplyFailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
