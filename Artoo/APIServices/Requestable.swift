e5b1092b4e8ac7b7b73c5f6e6ed128251512e460
보윤 <bykim94@hanmail.net>
2019-01-09 15:53:58 +0900

    댓글 삭제와 뷰 작업 조금

--- Artoo/APIServices/Requestable.swift
+++ Artoo/APIServices/Requestable.swift
@@ -63,6 +63,20 @@ extension Requestable {
         
     }
     
+    func delettable(_ url: String, body: [String:Any]?, header: HTTPHeaders?, completion: @escaping (NetworkResult<NetworkData>) -> Void) {
+        Alamofire.request(url, method: .delete, parameters: body, encoding: JSONEncoding.default, headers: header).responseObject { (res: DataResponse<NetworkData>) in
+            switch res.result {
+            case .success:
+                guard let value = res.result.value else { return }
+                completion(.success(value))
+            case .failure:
+                guard let value = res.result.value else { return }
+                completion(.error(value))
+            }
+        }
+        
+    }
+    
     func gettable(_ url: String, param: [String:Any]?, header: HTTPHeaders?, completion: @escaping (NetworkResult<NetworkData>) -> Void) {
         Alamofire.request(url, method: .get, parameters: param, encoding: JSONEncoding.default, headers: header).responseObject { (res: DataResponse<NetworkData>) in
             switch res.result {
