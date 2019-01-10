e8857c0b40aae3ee4933116ee148aa886e905afa
jmzzang <dream7739@naver.com>
2019-01-11 06:17:23 +0900

    메인 탭 UI 컬렉션으로 변경

--- Artoo/VC/Mypage/MyPageMainVC.swift
+++ Artoo/VC/Mypage/MyPageMainVC.swift
@@ -10,25 +10,40 @@
 import UIKit
 
 class MyPageMainVC: UIViewController {
-
+    
+    //탭 바로 사용할 컬렉션뷰
+    @IBOutlet weak var mypageCollectionView: UICollectionView!
+    
+    
     @IBOutlet weak var MainIntroductionLabel: UILabel!
     @IBOutlet weak var EditTextView: UITextView!
+    
+    //버튼 -> 컬렉션 뷰로 변경
     @IBOutlet weak var artBtn: UIButton!
     @IBOutlet weak var saveBtn: UIButton!
     @IBOutlet weak var buyBtn: UIButton!
     @IBOutlet weak var reviewBtn: UIButton!
+    
+    //내용 변경할 containerView
     @IBOutlet weak var containerView: UIView!
+    
     @IBOutlet weak var noticeView: UIView!
     
     var userInfo: String!
+    var tabInfo : [String:Int] = ["작품":0, "저장":0, "거래":0 , "후기":0]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         EditTextView.isScrollEnabled = false
         updateView(selected: 0)
-        noticeView.isHidden = true
+        // noticeView.isHidden = true
+        
+        //작품 저장 거래 후기
+        //컬렉션뷰 세팅
+        mypageCollectionView.delegate = self
+        mypageCollectionView.dataSource = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -70,38 +85,37 @@ class MyPageMainVC: UIViewController {
     }
     
     /*
-    //저장 버튼 눌렀을 때
-    @IBAction func SaveBtn(_ sender: Any) {
-        EditTextView.isEditable = false
-        MainIntroductionLabel.textColor = UIColor.darkGray
-        let userIndex = UserDefaults.standard.integer(forKey: "userIndex")
-        UserModifyService.shared.modifyUserInfo(user_idx: userIndex) {
-            (res) in guard let status = res.status else { return }
-            switch status {
-            case 200:
-                print("hi1")
-                //editable = true, 색깔 바꿔놓은 회색이 다시 검은색
-                //그 정보가 띄워지면 됨.
-                
-                //회원 정보 수정 성공
-            case 404:
-                print("hi2")
-                //유저 조회 실패
-            case 401:
-                print("hi3")
-                //회원 인증 실패
-            default:
-                print("hi")
-                
-            }
-            
-        }
-    }
-    */
-    //버튼에 따라서 아래 뷰가 바뀌기.
-    @IBAction func changeView(_ sender: UIButton) {
-        updateView(selected: sender.tag)
-    }
+     //저장 버튼 눌렀을 때
+     @IBAction func SaveBtn(_ sender: Any) {
+     EditTextView.isEditable = false
+     MainIntroductionLabel.textColor = UIColor.darkGray
+     let userIndex = UserDefaults.standard.integer(forKey: "userIndex")
+     UserModifyService.shared.modifyUserInfo(user_idx: userIndex) {
+     (res) in guard let status = res.status else { return }
+     switch status {
+     case 200:
+     print("hi1")
+     //editable = true, 색깔 바꿔놓은 회색이 다시 검은색
+     //그 정보가 띄워지면 됨.
+     
+     //회원 정보 수정 성공
+     case 404:
+     print("hi2")
+     //유저 조회 실패
+     case 401:
+     print("hi3")
+     //회원 인증 실패
+     default:
+     print("hi")
+     
+     }
+     
+     }
+     }
+     */
+    //    //버튼에 따라서 아래 뷰가 바뀌기.
+    //    @IBAction func changeView(_ sender: UIButton) {
+    //    }
     
     //art_page리턴
     private lazy var artPage: MyPageArtVC = {
@@ -149,6 +163,44 @@ class MyPageMainVC: UIViewController {
     }()
     
 }
+
+extension MyPageMainVC : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        updateView(selected: indexPath.row)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 48, height: 33.5)
+    }
+    
+    
+
+
+func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    
+    return 48
+}
+
+}
+
+extension MyPageMainVC : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tabInfo.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let data = Array(tabInfo.keys)
+        let count = Array(tabInfo.values)
+        let cell =  mypageCollectionView.dequeueReusableCell(withReuseIdentifier: "MyPageTabCell", for: indexPath) as! MyPageTabCell
+        cell.tabName.text = gsno(data[indexPath.row])
+        cell.tabCount.text = "\(gino(count[indexPath.row]))"
+        return cell
+    }
+    
+    
+}
+
 
 extension MyPageMainVC {
     
@@ -203,6 +255,6 @@ extension MyPageMainVC {
     }
     
     @objc func presentNoticeView() {
-        noticeView.isHidden = false
+        //    noticeView.isHidden = false
     }
 }
---
+++ Artoo/VC/Mypage/MyPageTabCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyPageTabCell.swift
+//  Artoo
+//
+//  Created by 홍정민 on 2019. 1. 11..
+//  Copyright © 2019년 홍정민. All rights reserved.
+//
+
+import UIKit
+
+class MyPageTabCell: UICollectionViewCell {
+    
+    @IBOutlet weak var indicatorImg: UIImageView!
+    @IBOutlet weak var tabName: UILabel!
+    @IBOutlet weak var tabCount: UILabel!
+    
+}
