353ef8c14d1a6605cc57a75ebdc1dfa1560a6a6e
jmzzang <dream7739@naver.com>
2019-01-11 18:32:14 +0900

    앱아이콘 변경/ 마이페이지 탭 변경 / 작품 컬렉션 서버연동

---
+++ Artoo/VC/Mypage/UpLoadNotiVC.swift
@@ -0,0 +1,38 @@
+//
+//  UpLoadNotiVC.swift
+//  Artoo
+//
+//  Created by 홍정민 on 2019. 1. 11..
+//  Copyright © 2019년 홍정민. All rights reserved.
+//
+
+import UIKit
+
+class UpLoadNotiVC: UIViewController {
+    
+ 
+    
+    @IBAction func dismissDialog(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    
+
+        override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
